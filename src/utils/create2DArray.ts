f78a2d43604bcb706e21eaa5e63541dc04b636c3
Aroma <aroma.xxx.777@gmail.com>
2024-01-18 22:57:41 +0900

    favicon

--- src/utils/create2DArray.ts
+++ src/utils/create2DArray.ts
@@ -5,16 +5,14 @@ import { getRandomBoolean } from './randomBoolean';
  * 配列の初期値は0か1のランダム
  */
 export const create2DArray = (row: number, column: number) => {
-  // 2次元配列の初期化
   const array2D = new Array(row);
   for (let i = 0; i < row; i++) {
     array2D[i] = new Array(column);
   }
 
-  // 2次元配列の要素を初期化（例：0で埋める）
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < column; j++) {
-      array2D[i][j] = getRandomBoolean(); // 初期値を変更したい場合はここを変更
+      array2D[i][j] = getRandomBoolean();
     }
   }
 
